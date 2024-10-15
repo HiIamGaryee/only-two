76a1a3faedaf5d391a815db831a7786543c476e9
HiIamGaryee <tgyandtpy@gmail.com>
2024-10-15 15:42:46 +0800

    add the ai bot part

--- src/Layout/index.tsx
+++ src/Layout/index.tsx
@@ -1,15 +1,21 @@
-import { ReactNode } from "react";
-import { Box } from "@mui/material";
+import { ReactNode, useState } from "react";
+import { Box, Dialog, Paper } from "@mui/material";
 import Sidebar from "../components/sidebar";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 // import Navbar from "../components/navbar";
+import AiBot from "../components/AiBot";
+import botImg from "../assets/go-green/bot-ai-img.jpg";
 
 interface LayoutProps {
   children: ReactNode;
 }
 
 const Layout = ({ children }: LayoutProps) => {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
   return (
     <Box
       sx={{
@@ -26,6 +32,32 @@ const Layout = ({ children }: LayoutProps) => {
         {children}
         <Footer />
       </Box>
+      <Paper
+        component="img"
+        onClick={handleOpen}
+        src={botImg}
+        sx={{
+          position: "fixed",
+          cursor: "pointer",
+          bottom: 15,
+          right: 30,
+          zIndex: 1000,
+          maxHeight: "80px",
+          borderRadius: "20px",
+          animation: "bounce 2s infinite",
+          "@keyframes bounce": {
+            "0%, 100%": {
+              transform: "translateY(0)",
+            },
+            "50%": {
+              transform: "translateY(-10px)",
+            },
+          },
+        }}
+      />
+      <Dialog open={open} onClose={handleClose} fullWidth maxWidth="sm">
+        <AiBot onClose={handleClose} />
+      </Dialog>
     </Box>
   );
 };
